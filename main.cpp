78d5dfd52fb28c6000dd8097a090cae85e04fa76
negative-seven <tehseven1@gmail.com>
2016-11-25 20:07:38 +0100

    Moved to Win32 application.
    No more .NET, woo!

--- main.cpp
+++ main.cpp
@@ -1,13 +1,137 @@
-#include "window.h"
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
-[System::STAThread]
-int WINAPI WinMain(HINSTANCE inst, HINSTANCE prev, LPSTR cmd, int show)
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <tchar.h>
+#include <windows.h>
+
+#include "randomize.h"
+#include "randomize_game.h"
+#include "utils.h"
+
+#define ID_INPUTBOX 0
+#define ID_CHECKBOX 1
+#define ID_BUTTON 2
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	// Open window
+	PAINTSTRUCT paintstruct;
+	HINSTANCE hInstance;
+	HWND inputbox, checkbox, button;
+	HFONT font;
 
-	System::Windows::Forms::Application::EnableVisualStyles();
-	System::Windows::Forms::Application::SetCompatibleTextRenderingDefault(false);
-	System::Windows::Forms::Application::Run(gcnew SuperMeatBoyrandomizer::window);
+	switch (message)
+	{
+	case WM_CREATE:
+		hInstance = (HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE);
+		font = CreateFont(14, 5, 0, 0, 500, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, ANTIALIASED_QUALITY, FF_DONTCARE, __T("Microsoft Sans Serif"));
+		
+		inputbox = CreateWindow("edit", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER, 10, 10, 84, 19, hWnd, (HMENU)ID_INPUTBOX, hInstance, NULL);
+		SendMessage(inputbox, WM_SETFONT, WPARAM(font), TRUE);
+		SendMessage(inputbox, EM_SETLIMITTEXT, WPARAM(10), NULL);
+		checkbox = CreateWindow("button", "Use random seed", WS_VISIBLE | WS_CHILD | BS_CHECKBOX, 100, 10, 150, 20, hWnd, (HMENU)ID_CHECKBOX, hInstance, NULL);
+		SendMessage(checkbox, WM_SETFONT, WPARAM(font), TRUE);
+		button = CreateWindow("button", "Randomize", WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 9, 35, 192, 23, hWnd, (HMENU)ID_BUTTON, hInstance, NULL);
+		SendMessage(button, WM_SETFONT, WPARAM(font), TRUE);
+		
+	case WM_PAINT:
+		BeginPaint(hWnd, &paintstruct);
+		EndPaint(hWnd, &paintstruct);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_COMMAND:
+		switch (HIWORD(wParam))
+		{
+		case EN_CHANGE:
+			switch (LOWORD(wParam))
+			{
+			case ID_INPUTBOX:
+				char text[16] = "";
+				DWORD selection_start = 0, selection_end = 0;
+				inputbox = GetDlgItem(hWnd, ID_INPUTBOX);
+				SendMessage(inputbox, EM_GETSEL, (WPARAM)&selection_start, (LPARAM)&selection_end);
+				SendMessage(inputbox, WM_GETTEXT, 16, (LPARAM)text);
+				SendMessage(inputbox, WM_SETTEXT, NULL, (LPARAM)limitInputbox(text).c_str());
+				SendMessage(inputbox, EM_SETSEL, (WPARAM)&selection_start, (LPARAM)&selection_end);
+			}
+		}
+		case BN_CLICKED:
+			switch (LOWORD(wParam))
+			{
+			case ID_BUTTON:
+				int seed;
+				
+				checkbox = GetDlgItem(hWnd, ID_CHECKBOX);
+				if (SendMessage(checkbox, BM_GETCHECK, NULL, NULL))
+				{
+					seed = randomInt();
+				}
+				else
+				{
+					char seed_temp[16] = "";
+					SendMessage(inputbox, WM_GETTEXT, 16, (LPARAM)seed_temp);
+					seed = (unsigned int)seed_temp;
+				}
+				randomize_game(seed);
+				break;
+
+			case ID_CHECKBOX:
+				checkbox = GetDlgItem(hWnd, ID_CHECKBOX);
+				SendMessage(checkbox, BM_SETCHECK, !SendMessage(checkbox, BM_GETCHECK, NULL, NULL), NULL);
+				break;
+			}
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wndClassEx;
+	static TCHAR window[] = "smb-randomizer";
+	static TCHAR window_title[] = "SMB randomizer";
+	HWND hWnd;
+	MSG msg;
+	
+	wndClassEx.cbSize = sizeof(WNDCLASSEX);
+	wndClassEx.style = CS_HREDRAW | CS_VREDRAW;
+	wndClassEx.lpfnWndProc = WndProc;
+	wndClassEx.cbClsExtra = 0;
+	wndClassEx.cbWndExtra = 0;
+	wndClassEx.hInstance = hInstance;
+	wndClassEx.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wndClassEx.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClassEx.hbrBackground = CreateSolidBrush(RGB(0xF0, 0xF0, 0xF0));
+	wndClassEx.lpszMenuName = NULL;
+	wndClassEx.lpszClassName = window;
+	wndClassEx.hIconSm = LoadIcon(wndClassEx.hInstance, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wndClassEx))
+	{
+		return 1;
+	}
+
+	hWnd = CreateWindow(window, window_title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 226, 106, NULL, NULL, hInstance, NULL);
+
+	if (!hWnd)
+	{
+		return 2;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
 }
---
+++ utils.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <windows.h>
+
+std::string limitInputbox(std::string string);
